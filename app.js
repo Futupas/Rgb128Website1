885a4f04b9c3af164aaa74a8e1ff51c93c07cd28
Olexandr Pascal <futupas@gmail.com>
2022-11-10 08:11:27 +0000

    add: basic blocks

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const galleryArts = document.querySelectorAll('#gallery > .art-preview');
+
+function checkSimpleDiv() {
+    const div = document.getElementById('simple');
+    if (!div || div.classList.contains('removing')) {
+        window.removeEventListener('scroll', checkSimpleDiv);
+        return;
+    }
+    simple.classList.add('removing');
+    setTimeout(() => {
+        simple.remove();
+    }, 1000);
+}
+window.addEventListener('scroll', checkSimpleDiv);
+
+function checkGalleryDiv() {
+    const artPreview = galleryArts[0];
+    const { offsetTop, offsetHeight } = galleryArts[0];
+    const screenHeight = document.documentElement.clientHeight;
+    const scrollY = window.scrollY;
+
+    const isTop = offsetTop + offsetHeight <= scrollY;
+    const isBottom = offsetTop >= scrollY + screenHeight;
+
+    isTop ? artPreview.classList.add('top') : artPreview.classList.remove('top');
+    isBottom ? artPreview.classList.add('bottom') : artPreview.classList.remove('bottom');
+}
+window.addEventListener('scroll', checkGalleryDiv);
